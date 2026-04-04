638e123397e2a56aa2d3a1dcd1f60bfcf2182082
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-04-03 22:53:56 -0400

    ckpt wip + cmd scratch buffer

--- server/src/acr/acr_cmd_checkpoint.c
+++ server/src/acr/acr_cmd_checkpoint.c
@@ -11,6 +11,9 @@
 
 #define ACR_INTERNAL
 #include "acr/acr.h"
+#include "afv/afv.h"
+#include "afv/afv_file.h"
+#include "afv/afv_metadata.h"
 #include "aim.h"
 #include "log.h"
 
@@ -29,63 +32,101 @@ void *acr_cmd_checkpoint(void *arg) {
     struct aurora_command_ctx *pCtx = arg;
     aim_entry_t *pInstance = pCtx->pInstance;
 
-    int acn_status = 0;
-    acn_status = acn_await(pInstance->pACN, eACN_memory);
-    if (acn_status != 0) {
-        log_error("ACN Error");
-    }
-
-    // Setup the checkpoint
-    char name[ACN_NAME_LEN];
-    int64_t version;
-    acn_get(pInstance->pACN, eACN_version, (uint64_t *) &version);
-    acn_get_name(pCtx->pInstance->pACN, name);
-    size_t arm_n_regions = arm_get_n_remote_regions(pInstance->pARM);
-    const amr_hndl *arm_regions = arm_get_remote_regions(pInstance->pARM);
-    const afv_metadata_t *pMetadata_old = afv_get_metadata(pInstance->pAFV);
-
-    // Setup Metadata
-    afv_metadata_t *pMetadata = afv_create_metadata(arm_n_regions);
-    if (!pMetadata) {
-        log_error("Bad Alloc?");
-        if (aim_enqueue(pCtx->pAIM, pCtx->pInstance) != 0) {
-            log_fatal("Could Not Enqueue! A thread lost a connection");
+    { // BEGIN Wait for outstanding memory operations to complete
+        int acn_status = 0;
+        acn_status = acn_await(pInstance->pACN, eACN_memory);
+        if (acn_status != 0) {
+            log_error("ACN Error");
+            goto CHECKPOINT_FAIL;
         }
-        return NULL;
-    }
+    } // END Wait for outstanding memory operations to complete
 
-    // Complete the checkpoint
-    for (size_t i = 0; i < arm_n_regions; i++) {
-        const amr_hndl *pAMR = &arm_regions[i];
+    afv_metadata_t *pMetadata = NULL;
 
-        pMetadata->region_ids[i] = pAMR->id;
-        pMetadata->region_sizes[i] = pAMR->rgn_size;
-        memcpy(pMetadata->region_names[i], pAMR->name, ARM_NAME_LEN);
-        log_trace("rgn: %d -> rgnid: %d (%d)", i, pAMR->id, pAMR->rgn_size);
+    { // BEGIN setup metadata
+        size_t arm_n_regions = arm_get_n_remote_regions(pInstance->pARM);
 
-        uint64_t test_data[4] = {0x0000, 0x1111, 0x2222, 0x3333};
-        eARM_error arm_status =
-            arm_read(pInstance->pARM, pAMR, pAMR->pShadow_memory, test_data,
-                     pAMR->rgn_size);
-        if (arm_status != eARM_OK) {
-            log_error("ARM Err %d", arm_status);
+        pMetadata = afv_create_metadata(arm_n_regions);
+
+        if (!pMetadata) {
+            log_error("Bad Alloc??");
+            goto CHECKPOINT_FAIL;
         }
-        for (int i = 0; i < 4; i++)
-            log_trace("TD %d 0x%lx", i, test_data[i]);
-    }
 
-    // Finalize the Checkpoint
-    pMetadata->rank = pMetadata_old->rank;
-    pMetadata->version = version;
-    pMetadata->n_regions = arm_n_regions;
-    memcpy(pMetadata->chkpt_name, name, ACN_NAME_LEN);
+        eACN_error acn_status = eACN_OK;
+        acn_status = acn_get(pInstance->pACN, eACN_version,
+                             (uint64_t *) &pMetadata->version);
+        if (acn_status != eACN_OK) {
+            afv_destroy_metadata(&pMetadata);
+            log_error("ACN Error 0x%lx", acn_status);
+            goto CHECKPOINT_FAIL;
+        }
+        acn_get_name(pCtx->pInstance->pACN, pMetadata->chkpt_name);
+        if (acn_status != eACN_OK) {
+            afv_destroy_metadata(&pMetadata);
+            log_error("ACN Error 0x%lx", acn_status);
+            goto CHECKPOINT_FAIL;
+        }
+        pMetadata->n_regions = arm_n_regions;
+        pMetadata->rank = afv_get_rank(pInstance->pAFV);
 
-    log_debug("checkpoint: %d %.*s", version, ACN_NAME_LEN, name);
+    } // END setup metadata
+
+    { // BEGIN Checkpoint
+        // Setup checkpoint file
+        afv_file_hndl *pCkpt_file =
+            afv_file_open_w(pInstance->pAFV, pMetadata->version,
+                            pMetadata->chkpt_name);
+
+        if (!pCkpt_file) {
+            afv_destroy_metadata(&pMetadata);
+            log_error("Bad Alloc??");
+            goto CHECKPOINT_FAIL;
+        }
+
+        // Read Only Pointer
+        // Do not Free
+        const amr_hndl *const arm_regions =
+            arm_get_remote_regions(pInstance->pARM);
+        if (!arm_regions) {
+            log_error("ARM Error");
+            goto CHECKPOINT_FAIL;
+        }
+
+        // Complete the checkpoint
+        for (size_t i = 0; i < pMetadata->n_regions; i++) {
+            const amr_hndl *pAMR = &arm_regions[i];
+
+            // Setup Region Metadata
+            pMetadata->region_ids[i] = pAMR->id;
+            pMetadata->region_sizes[i] = pAMR->rgn_size;
+            memcpy(pMetadata->region_names[i], pAMR->name, ARM_NAME_LEN);
+
+            log_debug("rgn: %d -> rgnid: %d (%d)", i, pAMR->id, pAMR->rgn_size);
+
+            uint64_t test_data[4] = {0x0000, 0x1111, 0x2222, 0x3333};
+            eARM_error arm_status =
+                arm_read(pInstance->pARM, pAMR, pAMR->pShadow_memory, test_data,
+                         pAMR->rgn_size);
+            if (arm_status != eARM_OK) {
+                log_error("ARM Err %d", arm_status);
+            }
+            for (int i = 0; i < 4; i++)
+                log_trace("TD %d 0x%lx", i, test_data[i]);
+        }
+
+        afv_file_close(&pCkpt_file);
+
+    } // END Checkpoint
+
+    // log_debug("checkpoint: %d %.*s", version, ACN_NAME_LEN, name);
 
     afv_write_metadata(pInstance->pAFV, pMetadata);
 
     // NOP
     acn_tick(pInstance->pACN, eACN_checkpoint);
+
+CHECKPOINT_FAIL:
 
     // Deal with AIM
     if (aim_enqueue(pCtx->pAIM, pCtx->pInstance) != 0) {
