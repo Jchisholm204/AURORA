dd2893e50c3626f1973a019e37da4b63e115d32b
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-04-11 20:52:00 -0400

    setup script

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,11 +1,43 @@
 #!/usr/bin/env zsh
 
+# Arch Independent Environment Setup Script
+# 
+
+local ARCH_OPTIONS=( "aarch64" "x86_64" "none" )
+
 function setup_env() {
     local ARCH=$1
+
+    echo "Setting up $ARCH environment"
+
     export MODULEPATH="/global/software/rocky-9.$ARCH/modfiles/langs:/global/software/rocky-9.$ARCH/modfiles/tools:/global/software/rocky-9.$ARCH/modfiles/apps:/etc/modulefiles:/usr/share/modulefiles"
 
-    echo $ARCH
+    module purge
+
+    module load gcc/11
+
+    module load hpcx/2.20
+    
+    return 0
 }
 
-ARCH=$(uname -m)
-setup_env $ARCH
+# Default Option - Use Host Arch
+local ARCH=$(uname -m)
+
+if [[ $1 ]]; then
+    # Check 
+    if [[ ${ARCH_OPTIONS[(r)$1]} == $1 ]]; then
+        ARCH=$1
+    else
+        echo "Invalid Arch Option. "
+        echo "Valid Options = {$ARCH_OPTIONS}"
+        return -1
+    fi
+fi
+
+if [[ $ARCH == "none" ]]; then
+    return 0
+else
+    setup_env $ARCH
+fi
+
