dd1f8a34f32e54e831d6b381221d23ddec52a3c3
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-04-11 23:08:23 -0400

    ucm

--- cmake/FindUCX.cmake
+++ cmake/FindUCX.cmake
@@ -9,14 +9,26 @@ if(NOT ucx_FOUND)
     )
     find_library(UCX_UCP_LIB NAMES ucp HINTS $ENV{UCX_HOME} PATH_SUFFIXES lib lib64)
     find_library(UCX_UCT_LIB NAMES uct HINTS $ENV{UCX_HOME} PATH_SUFFIXES lib lib64)
+    find_library(UCX_UCM_LIB NAMES ucm HINTS $ENV{UCX_HOME} PATH_SUFFIXES lib lib64)
     find_library(UCX_UCS_LIB NAMES ucs HINTS $ENV{UCX_HOME} PATH_SUFFIXES lib lib64)
+
+    find_library(Z_LIB NAMES z)
 
     if(UCX_INCLUDE_DIR AND UCX_UCP_LIB)
         add_library(${CMAKE_PROJECT_NAME}::ucx INTERFACE IMPORTED)
-        target_include_directories(${CMAKE_PROJECT_NAME}::ucx
-            INTERFACE ${UCX_INCLUDE_DIR})
-        target_link_libraries(${CMAKE_PROJECT_NAME}::ucx 
-            INTERFACE ${UCX_UCP_LIB} ${UCX_UCT_LIB} ${UCX_UCS_LIB})
+        target_include_directories(${CMAKE_PROJECT_NAME}::ucx INTERFACE 
+            ${UCX_INCLUDE_DIR}
+        )
+        target_link_libraries(${CMAKE_PROJECT_NAME}::ucx INTERFACE 
+            ${UCX_UCP_LIB} 
+            ${UCX_UCT_LIB} 
+            ${UCX_UCS_LIB} 
+            ${UCX_UCM_LIB} 
+            ${Z_LIB}
+            pthread
+            dl
+            m
+        )
         set(UCX_FOUND TRUE)
     else()
         message(WARNING "Failed to find UCX")
