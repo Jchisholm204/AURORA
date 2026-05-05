9790693571d0385cf28914390f75abccda592d24
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-05-04 22:27:05 -0400

    sbatch issues..

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -14,9 +14,14 @@ export ATH_JOB_NAME="test_job"
 export ATH_JOB_NODE_COUNT="$((((${#${(s:,:)ATH_BACKEND_NODES}} + ${#${(s:,:)ATH_TEST_NODES}}))))"
 # Convert array into CSV format
 export ATH_JOB_NODE_LIST=$( IFS=',';  echo "${ATH_NODES[*]}" )
-export ATH_JOB_TIME="0:20:0"
+export ATH_JOB_TIME="00:02:00"
 
 echo "$ATH_JOB_NODE_COUNT"
-# sbatch --output="idk.log" 
-${AURORA_TESTS_DIR}/test.batch
+sbatch --export \
+    --output="idk.log" \
+    --job-name=${ATH_JOB_NAME} \
+    --nodes=${ATH_JOB_NODE_COUNT} \
+    --nodelist=${ATH_JOB_NODE_LIST} \
+    --time=${ATH_JOB_TIME} \
+    ${AURORA_TESTS_DIR}/test.batch
 
