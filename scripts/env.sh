5d7b6475ad9369e053186e4d2b2dec3418f970f5
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-05-07 23:41:59 -0700

    testing wip

--- scripts/env.sh
+++ scripts/env.sh
@@ -13,79 +13,111 @@ AURORA_LOG_DIR="results/0.0.1-1"
 
 # -- END User Environment Variables -- 
 
-# -- BEGIN Directory/ENV Setup -- 
-
-# Find the toplevel of the project
-if [[ $(git rev-parse --is-inside-work-tree) == true ]]; then
-    export AURORA_TOPLEVEL=$(git rev-parse --show-toplevel)
-else
-    # Compatibility layer for tarballs
-    echo "WARNING: Git repository not detected.\n  Using: $(pwd) as the top level directory."
-    export AURORA_TOPLEVEL=$(pwd)
-fi
-
-# All AURORA paths declared here
-# Scripts ALWAYS use these paths rather than relative paths
-export AURORA_SCRIPT_DIR="${AURORA_TOPLEVEL}/scripts"
-export AURORA_TESTS_DIR="${AURORA_SCRIPT_DIR}/tests"
-export AURORA_LAUNCH_DIR="${AURORA_SCRIPT_DIR}/launchers"
-export AURORA_CLUSTER_DIR="${AURORA_SCRIPT_DIR}/clusters/${AURORA_CLUSTER_NAME}"
-
-# Fix Checkpointing Directory (user ENV)
-if [[ $AURORA_CHECKPOINT_DIR[1] = "/" ]]; then
-    export AURORA_CHECKPOINT_DIR=$AURORA_CHECKPOINT_DIR
-else
-    export AURORA_CHECKPOINT_DIR="$AURORA_TOPLEVEL/$AURORA_CHECKPOINT_DIR"
-fi
-
-# Fix Logging Directory (user ENV)
-if [[ $AURORA_LOG_DIR[1] = "/" ]]; then
-    export AURORA_LOG_DIR=$AURORA_LOG_DIR
-else
-    export AURORA_LOG_DIR="$AURORA_TOPLEVEL/$AURORA_LOG_DIR"
-fi
-
-# -- Validity Checks --
-
-# Check the checkpoint directory is valid
-if [[ ! -d $AURORA_CHECKPOINT_DIR ]]; then
-    mkdir -p "${AURORA_CHECKPOINT_DIR}"
-    if [[ $? -ne 0 ]]; then
-        echo "Warning: Specified Checkpoint Directory Unavaliable. Tests may fail." >&2
+function ath_setup_basedir(){
+    # Find the toplevel of the project
+    if [[ $(git rev-parse --is-inside-work-tree) == true ]]; then
+        export AURORA_TOPLEVEL=$(git rev-parse --show-toplevel)
+    else
+        # Compatibility layer for tarballs
+        printf "WARNING: Git repository not detected." >& 2
+        printf "Using: %s as the top level directory." $(pwd) >& 2
+        export AURORA_TOPLEVEL=$(pwd)
     fi
-fi
+}
 
-# Check the logging directory is valid
-if [[ ! -d $AURORA_LOG_DIR ]]; then
-    mkdir -p "${AURORA_LOG_DIR}"
-    if [[ $? -ne 0 ]]; then
-        echo "Warning: Specified Logging Directory Unavaliable. Tests may fail." >&2
+function ath_locate_directory(){
+    local DIR=$1
+    if [[ $DIR[1] == "/" ]]; then
+        local DIR=$DIR
+    else
+        local DIR="${AURORA_TOPLEVEL}/${DIR}"
     fi
-fi
 
-if [[ ! -d $AURORA_CLUSTER_DIR ]]; then
-    echo "Error: Cluster Directory Not Found" >&2
-    echo "Cluster Directory must contain cluster configuration scripts." >&2
-    return 1
-fi
+    echo "$DIR"
 
+    if [[ ! -d $DIR ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
 
-# -- END Directory Setup -- 
+function ath_setup_directories(){
+    # All AURORA paths declared here
+    # Scripts ALWAYS use these paths rather than relative paths
+    export AURORA_SCRIPT_DIR="${AURORA_TOPLEVEL}/scripts"
+    export AURORA_TESTS_DIR="${AURORA_SCRIPT_DIR}/tests"
+    export AURORA_LAUNCH_DIR="${AURORA_SCRIPT_DIR}/launchers"
+    export AURORA_CLUSTER_DIR="${AURORA_SCRIPT_DIR}/clusters/${AURORA_CLUSTER_NAME}"
 
-# -- BEGIN Source External Scripts -- 
+    # Fix Checkpointing Directory (user ENV)
+    if [[ $AURORA_CHECKPOINT_DIR[1] = "/" ]]; then
+    else
+        export AURORA_CHECKPOINT_DIR="$AURORA_TOPLEVEL/$AURORA_CHECKPOINT_DIR"
+    fi
 
-if [[ ! -f "$AURORA_CLUSTER_DIR/env.sh" ]]; then
-    echo "Error: Cluster ENV script not found" >&2
-    return 1
-fi
+    export AURORA_CHECKPOINT_DIR=$( ath_locate_directory "$AURORA_CHECKPOINT_DIR" )
 
-if [[ ! -f "$AURORA_CLUSTER_DIR/launch_config.sh" ]]; then
-    echo "Error: Cluster Launch Configuration script not found" >&2
-    return 1
-else
+    export AURORA_LOG_DIR=$( ath_locate_directory "$AURORA_LOG_DIR" )
+}
+
+function ath_verify_directories(){
+    # Check the checkpoint directory is valid
+    if [[ ! -d $AURORA_CHECKPOINT_DIR ]]; then
+        mkdir -p "${AURORA_CHECKPOINT_DIR}"
+        if [[ $? -ne 0 ]]; then
+            printf "Warning: Specified Checkpoint Directory Unavaliable.\n" >& 2
+            printf "Tests may fail.\n" >&2
+        fi
+    fi
+
+    # Check the logging directory is valid
+    if [[ ! -d $AURORA_LOG_DIR ]]; then
+        mkdir -p "${AURORA_LOG_DIR}"
+        if [[ $? -ne 0 ]]; then
+            printf "Warning: Specified Logging Directory Unavaliable.\n" >& 2
+            printf "Tests may fail.\n" >&2
+        fi
+    fi
+}
+
+function ath_verify_cluster_config(){
+    local CLUSTER_NAME=$1
+    local CLUSTER_DIR="${AURORA_SCRIPT_DIR}/clusters/${CLUSTER_NAME}"
+    if [[ ! -d $CLUSTER_DIR ]]; then
+        printf "Error: Cluster Directory Not Found\n" >&2
+        printf "Cluster Directory must contain cluster configuration scripts.\n" >&2
+        return 1
+    fi
+
+    if [[ ! -f "$CLUSTER_DIR/env.sh" ]]; then
+        printf "Error: Cluster ENV script not found\n" >&2
+        return 1
+    fi
+    if [[ ! -f "$CLUSTER_DIR/launch_config.sh" ]]; then
+        printf "Error: Cluster Launch Configuration script not found\n" >&2
+        return 2
+    fi
+}
+
+function ath_source_cluster_config(){
+    local CLUSTER_NAME=$1
+    local CLUSTER_DIR="${AURORA_SCRIPT_DIR}/clusters/${CLUSTER_NAME}"
+    ath_verify_cluster_config "$CLUSTER_NAME"
     source $AURORA_CLUSTER_DIR/launch_config.sh
     # Must compress the array into a string to share across scripts
     export ATH_NODES_STR="${(j: :)ATH_NODES}"
-fi
+}
 
-# -- END Source External Scripts -- 
+
+function launch_test(){
+    
+}
+
+
+ath_setup_basedir
+ath_setup_directories
+ath_verify_directories
+if ath_verify_cluster_config $AURORA_CLUSTER_NAME; then
+    ath_source_cluster_config $AURORA_CLUSTER_NAME
+fi
