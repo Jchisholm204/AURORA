03b342ca3814227eb6b378cbb3b1025bd1bf4cb3
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-05-08 20:27:17 -0700

    cleanup

--- scripts/env.sh
+++ scripts/env.sh
@@ -7,81 +7,78 @@
 #   offset from the installation directory
 
 
-AURORA_CHECKPOINT_DIR="checkpoints"
-AURORA_CLUSTER_NAME="rome"
-AURORA_LOG_DIR="results/0.0.1-1"
+local AURORA_CHECKPOINT_DIR="checkpoints"
+local AURORA_CLUSTER_NAME="rome"
+local AURORA_LOG_DIR="results/0.0.1-1"
 
 # -- END User Environment Variables -- 
 
-function ath_setup_basedir(){
-    # Find the toplevel of the project
+function _ath_finddir_basedir(){
+    # Helper function to find the base repository directory (pure)
+    # Returns by echo, return value indicates git (0) or fallback (1)
     if [[ $(git rev-parse --is-inside-work-tree) == true ]]; then
-        export AURORA_TOPLEVEL=$(git rev-parse --show-toplevel)
+        printf "%s" $(git rev-parse --show-toplevel)
+        return 0
     else
         # Compatibility layer for tarballs
         printf "WARNING: Git repository not detected." >& 2
         printf "Using: %s as the top level directory." $(pwd) >& 2
-        export AURORA_TOPLEVEL=$(pwd)
+        printf "%s" $(pwd)
+        return 1
     fi
 }
 
-function ath_locate_directory(){
+function _ath_finddir_abspath(){
+    # Converts a relative (within this repo) or absolute path
+    # to an absolute path. (pure)
+    # Returns by echo, value indicates success (0) or failure (1)
     local DIR=$1
     if [[ $DIR[1] == "/" ]]; then
         local DIR=$DIR
     else
-        local DIR="${AURORA_TOPLEVEL}/${DIR}"
+        local DIR="$(_ath_finddir_basedir)/${DIR}"
     fi
 
     echo "$DIR"
 
     if [[ ! -d $DIR ]]; then
+        printf "Error: Directory '%s' not found.\n" $DIR >& 2
         return 1
     else
         return 0
     fi
 }
 
-function ath_setup_directories(){
-    # All AURORA paths declared here
+function ath_setup_env(){
+    # All AURORA paths declared here (impure)
     # Scripts ALWAYS use these paths rather than relative paths
-    export AURORA_SCRIPT_DIR="${AURORA_TOPLEVEL}/scripts"
-    export AURORA_TESTS_DIR="${AURORA_SCRIPT_DIR}/tests"
-    export AURORA_LAUNCH_DIR="${AURORA_SCRIPT_DIR}/launchers"
-    export AURORA_CLUSTER_DIR="${AURORA_SCRIPT_DIR}/clusters/${AURORA_CLUSTER_NAME}"
-
-    # Fix Checkpointing Directory (user ENV)
-    if [[ $AURORA_CHECKPOINT_DIR[1] = "/" ]]; then
-    else
-        export AURORA_CHECKPOINT_DIR="$AURORA_TOPLEVEL/$AURORA_CHECKPOINT_DIR"
+    local AURORA_TOPLEVEL=$(_ath_finddir_basedir)
+    if [[ $? -ne 0 ]]; then
+        printf "Fallback: Using %s as the top level directory.\n" $AURORA_TOPLEVEL
     fi
 
-    export AURORA_CHECKPOINT_DIR=$( ath_locate_directory "$AURORA_CHECKPOINT_DIR" )
-
-    export AURORA_LOG_DIR=$( ath_locate_directory "$AURORA_LOG_DIR" )
-}
-
-function ath_verify_directories(){
-    # Check the checkpoint directory is valid
-    if [[ ! -d $AURORA_CHECKPOINT_DIR ]]; then
-        mkdir -p "${AURORA_CHECKPOINT_DIR}"
-        if [[ $? -ne 0 ]]; then
-            printf "Warning: Specified Checkpoint Directory Unavaliable.\n" >& 2
-            printf "Tests may fail.\n" >&2
-        fi
+    AURORA_SCRIPT_DIR="${AURORA_TOPLEVEL}/scripts"
+    AURORA_TESTS_DIR="${AURORA_SCRIPT_DIR}/tests"
+    AURORA_LAUNCH_DIR="${AURORA_SCRIPT_DIR}/launch"
+    AURORA_CLUSTER_DIR="${AURORA_SCRIPT_DIR}/clusters/${AURORA_CLUSTER_NAME}"
+    AURORA_CHECKPOINT_DIR=$(_ath_finddir_abspath "$AURORA_CHECKPOINT_DIR")
+    mkdir -p $AURORA_CHECKPOINT_DIR
+    if [[ $? -ne 0 ]]; then
+        printf "Warning: Invalid Checkpoint Directory.\n"
     fi
-
-    # Check the logging directory is valid
-    if [[ ! -d $AURORA_LOG_DIR ]]; then
-        mkdir -p "${AURORA_LOG_DIR}"
-        if [[ $? -ne 0 ]]; then
-            printf "Warning: Specified Logging Directory Unavaliable.\n" >& 2
-            printf "Tests may fail.\n" >&2
-        fi
+    AURORA_LOG_DIR=$(_ath_finddir_abspath "$AURORA_LOG_DIR")
+    mkdir -p $AURORA_LOG_DIR
+    if [[ $? -ne 0 ]]; then
+        printf "Warning: Invalid Logging Directory.\n"
     fi
+    export AURORA_SCRIPT_DIR AURORA_TESTS_DIR 
+    export AURORA_LAUNCH_DIR AURORA_CLUSTER_DIR 
+    export AURORA_CHECKPOINT_DIR
 }
 
 function ath_verify_cluster_config(){
+    # Verifies that the cluster configuration contains the correct scripts
+    # (impure) $1 - Cluster Name
     local CLUSTER_NAME=$1
     local CLUSTER_DIR="${AURORA_SCRIPT_DIR}/clusters/${CLUSTER_NAME}"
     if [[ ! -d $CLUSTER_DIR ]]; then
@@ -92,12 +89,13 @@ function ath_verify_cluster_config(){
 
     if [[ ! -f "$CLUSTER_DIR/env.sh" ]]; then
         printf "Error: Cluster ENV script not found\n" >&2
-        return 1
+        return 2
     fi
     if [[ ! -f "$CLUSTER_DIR/launch_config.sh" ]]; then
         printf "Error: Cluster Launch Configuration script not found\n" >&2
-        return 2
+        return 3
     fi
+    return 0
 }
 
 function ath_source_cluster_config(){
@@ -109,15 +107,7 @@ function ath_source_cluster_config(){
     export ATH_NODES_STR="${(j: :)ATH_NODES}"
 }
 
-
-function launch_test(){
-    
-}
-
-
-ath_setup_basedir
-ath_setup_directories
-ath_verify_directories
+ath_setup_env
 if ath_verify_cluster_config $AURORA_CLUSTER_NAME; then
     ath_source_cluster_config $AURORA_CLUSTER_NAME
 fi
