4a44848c9460be7e68f6b1266a6c35254691af2e
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-06-30 22:03:29 -0400

    veloc

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -25,6 +25,7 @@ function submit_test(){
     echo " NAME: ${JOB_NAME}"
     echo " TIME: ${JOB_TIME}"
     echo " SCRIPT: ${JOB_SCRIPT}"
+    # Launch without sbatch
     # ${AURORA_TESTS_DIR}/${JOB_SCRIPT} \
     #     $JOB_NAME \
     #     'rome005' \
@@ -32,21 +33,19 @@ function submit_test(){
     #     "${@[5,-1]}"
         # $JOB_TEST_NODES \
         # $JOB_BACKEND_NODES \
-    echo "EXPORTS:"
-    for EXPORT in ${AURORA_EXPORT_LIST}; do
-        echo "    ${EXPORT}: ${(P)EXPORT}"
-    done
+    # Launch with Sbatch
     sbatch --export=${(j:,:)AURORA_EXPORT_LIST} \
         --output="${AURORA_LOG_DIR}/${JOB_NAME}/slurm-%j.log" \
         --job-name=${JOB_NAME} \
         --nodes='2' \
-        --nodelist='rome005,romebf3a005' \
+        --nodelist='rome006,romebf3a006' \
         --time="${JOB_TIME}" \
         ${AURORA_TESTS_DIR}/${JOB_SCRIPT} \
             $JOB_NAME \
-            'rome005' \
-            'romebf3a005' \
+            'rome006' \
+            'romebf3a006' \
             "${@[5,-1]}"
+        # --nodelist='rome005,romebf3a005' \
 }
 
 # submit_test 'heat_distribution_aurora.zsh' '02:00:00' '2' \
@@ -62,12 +61,12 @@ function submit_test(){
 #     '16' \
 #     '16'
 
-submit_test 'heat_distribution_aurora.zsh' '02:00:00' '2' \
-    'heatdis_aurora8' \
-    10 \
-    '256' \
-    '32' \
-    '8'
+# submit_test 'heat_distribution_aurora.zsh' '02:00:00' '2' \
+#     'heatdis_aurora8' \
+#     10 \
+#     '256' \
+#     '32' \
+#     '8'
 # submit_test 'heat_distribution_aurora.zsh' '02:00:00' '2' \
 #     'heatdis_aurora16' \
 #     10 \
@@ -81,7 +80,7 @@ submit_test 'heat_distribution_aurora.zsh' '02:00:00' '2' \
 #     '256' \
 #     '64' \
 #     '16'
-
+#
 # submit_test 'heat_distribution_aurora.zsh' '02:00:00' '2' \
 #     'heatdis_aurora16' \
 #     10 \
@@ -90,3 +89,15 @@ submit_test 'heat_distribution_aurora.zsh' '02:00:00' '2' \
 #     '16'
 
 
+submit_test 'heat_distribution_veloc.zsh' '02:00:00' '2' \
+    'heatdis_veloc' \
+    10 \
+    '256' \
+    '32' \
+
+# submit_test 'heat_distribution_aurora.zsh' '02:00:00' '2' \
+#     'heatdis_aurora16' \
+#     10 \
+#     '256' \
+#     '128' \
+#     '16'
---
+++ scripts/tests/heat_distribution_veloc.zsh
@@ -0,0 +1,102 @@
+#!/usr/bin/env zsh
+
+# Heat Distribution VeloC Test
+
+# All scripts re-source the ENV
+source ${AURORA_SCRIPT_DIR}/env.sh
+
+local JOB_NAME=$1
+local LOG_DIR="${AURORA_LOG_DIR}/${JOB_NAME}"
+local TEST_NODES=$2
+local N_RUNS=$4
+local MEM_MB=$5
+local PROCS=$6
+
+echo "JOB: ${JOB_NAME}"
+echo "LOG_DIR: ${LOG_DIR}"
+echo "TEST_NODES: ${TEST_NODES}"
+echo "BACKEND_NODES: ${BACKEND_NODES}"
+echo "N_RUNS: ${N_RUNS}"
+echo "MEM_MB: ${MEM_MB}"
+echo "PROCS: ${PROCS}"
+echo "BACKEND_PROCS: ${BACKEND_PROCS}"
+
+local TMP_DIR=${AURORA_CLUSTER_TMP_DIR}/${JOB_NAME}_${MEM_MB}_${PROCS}_${N_RUNS}
+local CHECKPOINT_DIR=${AURORA_CLUSTER_CHECKPOINT_DIR}/${JOB_NAME}
+local TMP_CHECKPOINT_DIR="/tmp/${JOB_NAME}_${MEM_MB}_${PROCS}_${N_RUNS}"
+local BUILD_DIR=${TMP_DIR}/build
+
+function build_test_heat_distribution(){
+    echo "${AURORA_CLUSTER_TMP_DIR}"
+    mkdir -p "${BUILD_DIR}"
+    ${AURORA_LAUNCH_DIR}/build.zsh \
+        ${BUILD_DIR} \
+        ${BUILD_DIR} \
+        ${BACKEND_PROCS}
+
+    # Create the config file
+    cat << EOF > "${TMP_DIR}/veloc_heatdis.cfg"
+scratch = ${TMP_CHECKPOINT_DIR}
+persistent = ${CHECKPOINT_DIR}
+meta = ${CHECKPOINT_DIR}/meta
+max_versions = 2
+scratch_versions = 1
+mode = async
+chksum = false
+
+EOF
+}
+
+function setup_test_heat_distribution(){
+    mkdir -p $CHECKPOINT_DIR
+    rm -r $CHECKPOINT_DIR
+    mkdir -p $CHECKPOINT_DIR
+    mkdir -p $TMP_CHECKPOINT_DIR
+    rm -r $TMP_CHECKPOINT_DIR
+    mkdir -p $TMP_CHECKPOINT_DIR
+
+}
+
+function cleanup_test_heat_distribution(){
+    rm -rf $CHECKPOINT_DIR
+    rm -rf $TMP_CHECKPOINT_DIR
+}
+
+function run_test_heat_distribution(){
+    local ITERATION=$1
+    setup_test_heat_distribution
+
+    echo "Starting Heat Distribution Test:"
+    echo "ITERATION=$ITERATION"
+    ${AURORA_SCRIPT_DIR}/launch/launch_test_none.zsh \
+        "${JOB_NAME}_p${PROCS}_i${ITERATION}" \
+        "${LOG_DIR}" \
+        "${BUILD_DIR}/tests/heatdis_veloc_mem ${MEM_MB} ${TMP_DIR}/veloc_heatdis.cfg" \
+        "${TEST_NODES}" \
+        ${PROCS} \
+
+    echo "Test Completed... Starting Restore.."
+
+    ${AURORA_SCRIPT_DIR}/launch/launch_test_none.zsh \
+        "${JOB_NAME}_p${PROCS}_i${ITERATION}_restore" \
+        "${LOG_DIR}" \
+        "${BUILD_DIR}/tests/heatdis_veloc_mem ${MEM_MB} ${TMP_DIR}/veloc_heatdis.cfg" \
+        "${TEST_NODES}" \
+        ${PROCS} \
+
+    echo "Test Completed... Cleaning Environment.."
+    cleanup_test_heat_distribution
+}
+
+echo 'Starting Heat Distribution Test:'
+mkdir -p "${LOG_DIR}"
+mkdir -p "${TMP_DIR}"
+build_test_heat_distribution
+
+# Iterate over test conditions
+for ((i = 0; i < $N_RUNS; i++)); do
+    run_test_heat_distribution "${i}"
+done
+
+echo "Heat Distribution Test Concluded"
+
