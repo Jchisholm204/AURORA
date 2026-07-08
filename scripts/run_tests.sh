4aa1d9af7e56fe218d424d1fe6ed313ca838b9fd
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-07-08 06:45:15 -0700

    256mb benchmarks for 128 cores

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -29,8 +29,8 @@ function submit_test(){
     if [[ $DEF_LOCAL_LAUNCH ]]; then
         ${AURORA_TESTS_DIR}/${JOB_SCRIPT} \
             $JOB_NAME \
-            'rome006' \
-            'romebf3a006' \
+            'rome005' \
+            'romebf3a005' \
             "${@[5,-1]}"
     else
         # Launch with Sbatch
@@ -38,12 +38,12 @@ function submit_test(){
             --output="${AURORA_LOG_DIR}/${JOB_NAME}/slurm-%j.log" \
             --job-name=${JOB_NAME} \
             --nodes='2' \
-            --nodelist='rome006,romebf3a006' \
+            --nodelist='rome005,romebf3a005' \
             --time="${JOB_TIME}" \
             ${AURORA_TESTS_DIR}/${JOB_SCRIPT} \
             $JOB_NAME \
-            'rome006' \
-            'romebf3a006' \
+            'rome005' \
+            'romebf3a005' \
             "${@[5,-1]}"
     fi
 }
@@ -83,19 +83,19 @@ function submit_test(){
 #     '128' \
 #     '16'
 
-submit_test 'heat_distribution_aurora.zsh' '10:00:00' '2' \
-    'heatdis_aurora' \
-    10 \
-    '128' \
-    '128' \
-    '16'
-
 # submit_test 'heat_distribution_aurora.zsh' '10:00:00' '2' \
 #     'heatdis_aurora' \
 #     10 \
-#     '256' \
+#     '128' \
 #     '128' \
 #     '16'
+
+submit_test 'heat_distribution_aurora.zsh' '10:00:00' '2' \
+    'heatdis_aurora' \
+    10 \
+    '256' \
+    '128' \
+    '16'
 
 # submit_test 'heat_distribution_veloc.zsh' '08:00:00' '2' \
 #     'heatdis_veloc' \
@@ -119,16 +119,16 @@ submit_test 'heat_distribution_aurora.zsh' '10:00:00' '2' \
 #     'heatdis_veloc' \
 #     10 \
 #     '64' \
+#     '128'
+
+# submit_test 'heat_distribution_veloc.zsh' '10:00:00' '2' \
+#     'heatdis_veloc' \
+#     10 \
+#     '128' \
 #     '128'
 
 submit_test 'heat_distribution_veloc.zsh' '10:00:00' '2' \
     'heatdis_veloc' \
     10 \
-    '128' \
+    '256' \
     '128'
-
-# submit_test 'heat_distribution_veloc.zsh' '10:00:00' '2' \
-#     'heatdis_veloc' \
-#     10 \
-#     '256' \
-#     '128'
