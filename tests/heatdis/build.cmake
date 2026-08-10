174647a2bca1f54c93fa1a745403d8be62a2067f
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-08-09 22:18:23 -0400

    install wip

--- tests/heatdis/build.cmake
+++ tests/heatdis/build.cmake
@@ -15,7 +15,7 @@ target_link_libraries(heatdis_original PRIVATE
 add_executable(heatdis_aurora ${HEATDIS_WORKING_DIR}/heatdis_aurora.c)
 
 target_link_libraries(heatdis_aurora PRIVATE 
-    aul 
+    ${CMAKE_PROJECT_NAME}::aul
     ${CMAKE_PROJECT_NAME}::mpi 
     m
 )
