932da18beaaeead61fcccd4ce2a2536e51e0623e
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-08-10 09:41:11 -0400

    adjust test linking

--- tests/block_test/build.cmake
+++ tests/block_test/build.cmake
@@ -2,7 +2,7 @@
 add_executable(block_test_aurora block_test/block_test_aurora.c)
 
 target_link_libraries(block_test_aurora PRIVATE 
-    aul
+    ${CMAKE_PROJECT_NAME}::aul
     ${CMAKE_PROJECT_NAME}::mpi
     # gtest # (If you decide to use GoogleTest later)
 )
